3005741e1f178d4981870dc7915573e1a061b782
Eric Lanehart <eric@pushred.co>
2016-09-19 17:55:16 -0400

    Style title input

--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,8 @@
 const unorphan = require('unorphan');
 
+const Header = require('../components/header');
+
 document.addEventListener('DOMContentLoaded', () => {
   unorphan('h1, p');
+  Header();
 });
-
