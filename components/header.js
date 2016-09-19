dfbbae3df5a7bd347f453683bf8aba174ef34bdb
Eric Lanehart <eric@pushred.co>
2016-09-19 19:49:12 -0400

    Eradicate tabs
    EditorConfig fell asleep on the job

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 function makeEditable () {
   const headerContentEl = document.querySelector('.post__header_content');
 
-	headerContentEl.innerHTML = renderInput();
+  headerContentEl.innerHTML = renderInput();
   headerContentEl.classList.add('post__header_content--modify');
 
   const inputEl = document.querySelector('input', headerContentEl);
@@ -16,29 +16,29 @@ function makeEditable () {
 module.exports = makeEditable;
 
 function renderTitle (title = 'untitled') {
-	return `
-		<h1 class="post__header_title"><span>${title}</span></h1>
-		<button class="button button--modify" title="Change">
+  return `
+    <h1 class="post__header_title"><span>${title}</span></h1>
+    <button class="button button--modify" title="Change">
       <span>Change</span>
       <svg><use xlink:href="/files/bundle.svg#modify"></use></svg>
     </button>
-	`;
+  `;
 }
 
 function renderInput (title = 'untitled', slug = 'untitled') {
-	return `
-		<form class="form">
-			<label class="input__label" for="title">Title</label>
-			<input class="input__control" name="title" type="text" value="${title}" tabindex="1" />
-			<p class="input__text">slug: <var class="input__text--var">${slug}</var></p>
-			<button class="button button--save icon" title="Save" type="submit">
+  return `
+    <form class="form">
+      <label class="input__label" for="title">Title</label>
+      <input class="input__control" name="title" type="text" value="${title}" tabindex="1" />
+      <p class="input__text">slug: <var class="input__text--var">${slug}</var></p>
+      <button class="button button--save icon" title="Save" type="submit">
         <span>Save</span>
         <svg><use xlink:href="/files/bundle.svg#save"></use></svg>
       </button>
-			<button class="button button--destroy icon" title="Cancel">
+      <button class="button button--destroy icon" title="Cancel">
         <span>Cancel</span>
         <svg><use xlink:href="/files/bundle.svg#destroy"></use></svg>
       </button>
-		</form>
-	`;
+    </form>
+  `;
 }
