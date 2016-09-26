61ebeb8199db6284c2ede4c0534a1053054b7044
Eric Lanehart <eric@pushred.co>
2016-09-26 11:39:37 -0400

    Seed “Are we out of the woods yet?” post
    And educate some apostrophes

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,9 +4,10 @@ const parameterize = require('parameterize');
 const db = require('../db');
 
 [
+  "Are we out of the woods yet?",
   "It is impossible to walk rapidly and be unhappy.",
-  "We don't get offered crises, they arrive.",
-  "I have seen the future and it doesn't work.",
+  "We don’t get offered crises, they arrive.",
+  "I have seen the future and it doesn’t work.",
   "I dwell in possibility...",
   "Knowledge is power."
 ].forEach(title => {
